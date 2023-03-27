8ddfc61af251292deaf2f45d57bda76d844d899d
ThanhNguyen <trungthanh.nguyen@verticurl.com>
2023-03-27 13:54:29 +0700

    update

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -971,6 +971,122 @@ var BaseModule = /*#__PURE__*/function (_Publisher) {
 
 /***/ }),
 
+/***/ "./src/assets/js/modules/DisplayLeaderBoard.js":
+/*!*****************************************************!*\
+  !*** ./src/assets/js/modules/DisplayLeaderBoard.js ***!
+  \*****************************************************/
+/*! namespace exports */
+/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ DisplayLeaderBoard; }
+/* harmony export */ });
+/* harmony import */ var _BaseModule__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./BaseModule */ "./src/assets/js/modules/BaseModule.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+
+
+
+var DisplayLeaderBoard = /*#__PURE__*/function (_BaseModule) {
+  _inherits(DisplayLeaderBoard, _BaseModule);
+
+  var _super = _createSuper(DisplayLeaderBoard);
+
+  function DisplayLeaderBoard() {
+    _classCallCheck(this, DisplayLeaderBoard);
+
+    return _super.apply(this, arguments);
+  }
+
+  _createClass(DisplayLeaderBoard, [{
+    key: "register",
+    value: function () {
+      var _register = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return axios__WEBPACK_IMPORTED_MODULE_1___default()({
+                  url: 'https://us-central1-test-c1a7a.cloudfunctions.net/api/v1/leaderboard/0EejNWFdxllT92ZWmByW?top=10',
+                  method: 'GET',
+                  headers: {
+                    "x-api-key": "2GB3VQI-KLQUVKI-QKXS67Q-KIKL55Q"
+                  }
+                }).then(function (result) {
+                  var users = result.data.users;
+                  var sortedUsers = users.sort(function (a, b) {
+                    return b.score - a.score;
+                  });
+                  console.log('sortedUsers', sortedUsers);
+                  var listHtml = '';
+                  sortedUsers.forEach(function (user, idx) {
+                    if (idx < 10) {
+                      listHtml += " <li>\n              <div><span class=\"number\">".concat(idx + 1, "</span></div>\n              <div class=\"inner\">\n                <span>").concat(user.username, "</span>\n                <span class=\"country\">").concat(user.country, "</span>\n                <span class=\"speciality\">").concat(user.speciality, "</span>\n                <span>").concat(user.score, "</span>\n              </div>\n            </li>");
+                    }
+                  });
+                  console.log('listHtml', listHtml, document.querySelectorAll('.leader-list'));
+                  document.querySelectorAll('.leader-list').forEach(function (list) {
+                    list.innerHTML = listHtml;
+                  });
+                })["catch"](function (error) {
+                  console.log(error);
+                });
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function register() {
+        return _register.apply(this, arguments);
+      }
+
+      return register;
+    }()
+  }]);
+
+  return DisplayLeaderBoard;
+}(_BaseModule__WEBPACK_IMPORTED_MODULE_0__.default);
+
+
+
+/***/ }),
+
 /***/ "./src/assets/js/modules/HelloModule.js":
 /*!**********************************************!*\
   !*** ./src/assets/js/modules/HelloModule.js ***!
@@ -1209,6 +1325,8 @@ var RefState = /*#__PURE__*/function (_BaseModule) {
 var map = {
 	"./BaseModule": "./src/assets/js/modules/BaseModule.js",
 	"./BaseModule.js": "./src/assets/js/modules/BaseModule.js",
+	"./DisplayLeaderBoard": "./src/assets/js/modules/DisplayLeaderBoard.js",
+	"./DisplayLeaderBoard.js": "./src/assets/js/modules/DisplayLeaderBoard.js",
 	"./HelloModule": "./src/assets/js/modules/HelloModule.js",
 	"./HelloModule.js": "./src/assets/js/modules/HelloModule.js",
 	"./Slider": "./src/assets/js/modules/Slider.js",
---
+++ src/assets/js/modules/DisplayLeaderBoard.js
@@ -0,0 +1,41 @@
+import BaseModule from './BaseModule'
+import axios from 'axios'
+export default class DisplayLeaderBoard extends BaseModule {
+
+  async register() {
+    await axios({
+      url: 'https://us-central1-test-c1a7a.cloudfunctions.net/api/v1/leaderboard/0EejNWFdxllT92ZWmByW?top=10',
+      method: 'GET',
+      headers: {
+        "x-api-key": "2GB3VQI-KLQUVKI-QKXS67Q-KIKL55Q",
+      },
+    }).then(result => {
+      const users = result.data.users
+      const sortedUsers = users.sort((a, b) => b.score - a.score);
+      console.log('sortedUsers', sortedUsers);
+
+      let listHtml = ''
+      sortedUsers.forEach((user, idx) => {
+        if (idx < 10) {
+          listHtml += ` <li>
+              <div><span class="number">${idx + 1}</span></div>
+              <div class="inner">
+                <span>${user.username}</span>
+                <span class="country">${user.country}</span>
+                <span class="speciality">${user.speciality}</span>
+                <span>${user.score}</span>
+              </div>
+            </li>`
+        }
+      });
+      console.log('listHtml', listHtml, document.querySelectorAll('.leader-list'));
+      document.querySelectorAll('.leader-list').forEach(list => {
+        list.innerHTML = listHtml
+      })
+
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+}
