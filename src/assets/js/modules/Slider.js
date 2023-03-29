6f87f8be1f18d7fade78c4052cafd335caf38d76
ThanhNguyen <trungthanh.nguyen@verticurl.com>
2023-03-29 10:45:07 +0700

    update

--- src/assets/js/modules/Slider.js
+++ src/assets/js/modules/Slider.js
@@ -4,28 +4,16 @@ import 'swiper/swiper.min.css'
 export default class Slider extends BaseModule {
 
   register() {
-    Swiper.use([Autoplay])
+    Swiper.use([Autoplay, EffectFade])
     const autoSlide = ~~(this.el.getAttribute('data-autoslide')) || ''
     console.log('autoSlide', autoSlide);
-    const videos = this.el.querySelectorAll('video')
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
-    if (window.innerWidth > 1024 && !isMobile) {
-      videos.forEach(video => {
-        video.removeAttribute('muted');
-      });
-    }
 
     this.swiper = new Swiper(this.el, {
       slidesPerView: 1,
-      // effect:'fade',
-      // fadeEffect: {
-      //   crossFade: true
-      // },
       loop: true,
       autoplay: {
         delay: autoSlide || 10000,
-        stopOnLastSlide: true
+        disableOnInteraction: false,
       },
     })
 
@@ -41,30 +29,55 @@ export default class Slider extends BaseModule {
       const currentSlide = event.slides[event.activeIndex];
       const currentVideo = currentSlide.querySelector('video');
       const videoTimeLimit = ~~(currentVideo?.getAttribute('data-time-limit')) || ''
-      // const btnMuted = currentSlide.querySelector('button');
+      const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
       if (currentVideo) {
         currentVideo.play();
-        currentVideo.addEventListener('ended', (e) => {
-          this.swiper.slideNext();
+        this.swiper.autoplay.stop();
+        this.isStopAutoplay = true
+
+        if (window.innerWidth > 1024 && !isMobile) {
+          currentVideo.muted = false;
+        }
+        currentVideo.addEventListener('play', (e) => {
         });
-        // btnMuted.setAttribute('data-mute', currentVideo.muted)
-        // btnMuted.addEventListener('click', (e) => {
-        //   btnMuted.setAttribute('data-mute', !currentVideo.muted)
-        //   currentVideo.muted = !currentVideo.muted;
-        // });
+
+        currentVideo.addEventListener('pause', (e) => {
+          console.log('pause',this.isStopAutoplay);
+          this.isStopAutoplay && this.swiper.autoplay.start()
+        });
+        currentVideo.addEventListener('ended', (e) => {
+          this.handleVideoEnd()
+        });
+
         currentVideo.addEventListener('timeupdate', (e) => {
           if (videoTimeLimit && currentVideo.currentTime >= videoTimeLimit) {
-            this.swiper.slideNext();
-            currentVideo.pause();
-            currentVideo.currentTime = 0;
-            this.swiper.params.autoplay = {
-              delay: autoSlide || 10000,
-              stopOnLastSlide: true
-            };
+            this.handleVideoEnd(currentVideo)
           }
         });
       }
     });
+  }
+
+  handleVideoEnd(video) {
+    if (document.fullscreenElement) {
+      console.log('fullscreenElement');
+      document.exitFullscreen().then(() => {
+        this.swiper.slideNext();
+        this.swiper.autoplay.start();
+        if (video) {
+          video.pause();
+          video.currentTime = 0;
+        }
+      });
+    } else {
+      this.swiper.slideNext();
+      this.swiper.autoplay.start();
+      if (video) {
+        video.pause();
+        video.currentTime = 0;
+      }
+    }
+    this.isStopAutoplay = false
   }
 }
