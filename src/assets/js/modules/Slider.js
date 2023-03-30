2ee44906d09f5e6783c681a72e96426aa5d83e43
ThanhNguyen <trungthanh.nguyen@verticurl.com>
2023-03-30 18:12:52 +0700

    update

--- src/assets/js/modules/Slider.js
+++ src/assets/js/modules/Slider.js
@@ -28,16 +28,32 @@ export default class Slider extends BaseModule {
 
       const currentSlide = event.slides[event.activeIndex];
       const currentVideo = currentSlide.querySelector('video');
+      const btnPlayVideo = currentSlide.querySelector('button');
+
       const videoTimeLimit = ~~(currentVideo?.getAttribute('data-time-limit')) || ''
       const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      btnPlayVideo?.classList.add('hidden')
 
       if (currentVideo) {
-        currentVideo.play();
-        this.swiper.autoplay.stop();
-        this.isStopAutoplay = true
-
         if (window.innerWidth > 1024 && !isMobile) {
           currentVideo.muted = false;
+        }
+        this.swiper.autoplay.stop();
+        this.isStopAutoplay = true
+        const promise = currentVideo.play();
+        if (promise !== undefined) {
+          promise.then(_ => {
+            // Autoplay started!
+            // console.log("Autoplay started!");
+          }).catch(error => {
+            btnPlayVideo.classList.remove('hidden')
+            btnPlayVideo.addEventListener('click', (e) => {
+              currentVideo.play()
+              btnPlayVideo.classList.add('hidden')
+            });
+            // Autoplay was prevented.
+            // Show a "Play" button so that user can start playback.
+          });
         }
 
         currentVideo.addEventListener('pause', (e) => {
